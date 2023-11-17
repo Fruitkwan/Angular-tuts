4a3ca159b93be63c5843056ba931b1016c8ea351
Hicham Soujae <hisham.soujae@gmail.com>
2023-11-16 21:18:27 -0500

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Task 1',
+        day: 'May 1st at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Task 2',
+        day: 'May 2nd at 3:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Task 3',
+        day: 'May 3rd at 4:30pm',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: 'Task 4',
+        day: 'May 4th at 5:30pm',
+        reminder: true
+    }
+];
